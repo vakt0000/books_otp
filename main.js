12bc70b7269d8ed1061eed82f820554193960863
victor <medina.victor.m@protonmail.com>
2022-01-18 21:30:11 +0100

    Added a checkbok for books that have been read

--- main.js
+++ main.js
@@ -22,37 +22,40 @@ okAddBookButton.addEventListener('click', () => {
   const authorName = document.getElementById("author").value;
   const titleName = document.getElementById("title").value;
   const pagesNumber = document.getElementById("pages").value;
-  if (indexEditing[0] === undefined) createAddBook(authorName, titleName, pagesNumber);
-  else editBook(authorName, titleName, pagesNumber);
+  const read = document.getElementById("read").checked;
+  if (indexEditing[0] === undefined) createAddBook(authorName, titleName, pagesNumber, read);
+  else editBook(authorName, titleName, pagesNumber, read);
 })
 
 cancelAddBookButton.addEventListener('click', () => {
   removeOverlay();
 })
 
-function createAddBook(authorName, titleName, pagesNumber) {
-  addBookToLibrary(authorName, titleName, pagesNumber);
+function createAddBook(authorName, titleName, pagesNumber, read) {
+  addBookToLibrary(authorName, titleName, pagesNumber, read);
   removeOverlay();
   showNewBook();
 }
 
-function editBook(authorName, titleName, pagesNumber) {
+function editBook(authorName, titleName, pagesNumber, read) {
   myLibrary[indexEditing[0]].author = authorName;
   myLibrary[indexEditing[0]].title = titleName;
   myLibrary[indexEditing[0]].pages = pagesNumber;
+  myLibrary[indexEditing[0]].read = read;
   updateListBook();
   indexEditing.pop();
   removeOverlay();
 }
 
-function Book (title, author, pages) {
+function Book (title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
+  this.read = read
 }
 
-function addBookToLibrary (title, author, pages) {
-  myLibrary.push(new Book(title, author, pages));
+function addBookToLibrary (title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
 }
 
 function removeOverlay() {
@@ -70,9 +73,16 @@ function addOverlay() {
 
 function updateListBook() {
   const div = document.getElementById(indexEditing[0]);
-  div.querySelector(".author").textContent = `Author: ${myLibrary[indexEditing[0]].author}`
-  div.querySelector(".title").textContent = `Title: ${myLibrary[indexEditing[0]].title}`
-  div.querySelector(".pages").textContent = `Pages: ${myLibrary[indexEditing[0]].pages}`
+  div.querySelector(".author").textContent = `Author: ${myLibrary[indexEditing[0]].author}`;
+  div.querySelector(".title").textContent = `Title: ${myLibrary[indexEditing[0]].title}`;
+  div.querySelector(".pages").textContent = `Pages: ${myLibrary[indexEditing[0]].pages}`;
+  console.log(myLibrary[indexEditing[0]].read)
+  if (myLibrary[indexEditing[0]].read === true) {
+    div.querySelector(".read").textContent = 'Read'
+  }
+  else {
+    div.querySelector(".read").textContent = 'Not read'
+  }
 }
 
 function showNewBook() {
@@ -80,7 +90,8 @@ function showNewBook() {
   const author = myLibrary[indexNew].author;
   const title = myLibrary[indexNew].title;
   const pages = myLibrary[indexNew].pages;
-  const div = createFillInfoDiv(author, title, pages);
+  const read = myLibrary[indexNew].read;
+  const div = createFillInfoDiv(author, title, pages, read);
   div.appendChild(createEditButton())
   div.appendChild(createRemoveBookButton());
   div.classList.add("book-item")
@@ -88,20 +99,25 @@ function showNewBook() {
   listBooks.appendChild(div)
 }
 
-function createFillInfoDiv(author, title, pages) {
+function createFillInfoDiv(author, title, pages, read) {
   const div = document.createElement('div');
   const pAuthor = document.createElement('p');
   const pTitle = document.createElement('p');
   const pPages = document.createElement('p');
+  const pRead = document.createElement('p');
   pAuthor.classList.add("author");
   pTitle.classList.add("title");
-  pPages.classList.add("pages")
+  pPages.classList.add("pages");
+  pRead.classList.add("read");
   pAuthor.textContent = `Author: ${author}`;
   pTitle.textContent = `Title: ${title}`;
   pPages.textContent = `Pages: ${pages}`;
+  if (read === true) pRead.textContent = "Read";
+  else pRead.textContent = "Not read";
   div.appendChild(pAuthor);
   div.appendChild(pTitle);
   div.appendChild(pPages);
+  div.appendChild(pRead);
   return div;
 }
 
